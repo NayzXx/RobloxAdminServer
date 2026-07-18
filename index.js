6d91fdfb66fc8443557b63868bc3da271c8d58cb
NayzXx <ewittmar@gmail.com>
2026-07-18 14:14:24 +0200

    Ajout WebSocket Render

--- index.js
+++ index.js
@@ -1,32 +1,79 @@
+const http = require("http");
 const WebSocket = require("ws");
 
 const PORT = process.env.PORT || 3000;
 
-const wss = new WebSocket.Server({
-    port: PORT
+const server = http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end("Roblox Admin Server Online");
 });
 
-console.log("Serveur démarré.");
+const wss = new WebSocket.Server({
+    server,
+    path: "/ws"
+});
+
+let adminClients = [];
+let robloxServers = [];
+
+console.log("Serveur prêt.");
 
 wss.on("connection", (ws) => {
 
     console.log("Nouvelle connexion.");
 
-    ws.send(JSON.stringify({
-        type: "connected",
-        message: "Bienvenue"
-    }));
-
     ws.on("message", (message) => {
 
-        console.log(message.toString());
+        try {
+
+            const data = JSON.parse(message);
+
+            if (data.type === "register") {
+
+                if (data.client === "admin") {
+
+                    adminClients.push(ws);
+
+                    console.log("Application Windows connectée.");
+
+                }
+
+                else if (data.client === "roblox") {
+
+                    robloxServers.push(ws);
+
+                    console.log("Serveur Roblox connecté.");
+
+                }
+
+                ws.send(JSON.stringify({
+                    type: "registered"
+                }));
+
+            }
+
+        }
+        catch (err) {
+
+            console.log(err);
+
+        }
 
     });
 
     ws.on("close", () => {
 
-        console.log("Déconnexion.");
+        adminClients = adminClients.filter(x => x !== ws);
+        robloxServers = robloxServers.filter(x => x !== ws);
+
+        console.log("Client déconnecté.");
 
     });
+
+});
+
+server.listen(PORT, () => {
+
+    console.log(`Serveur lancé sur le port ${PORT}`);
 
 });
